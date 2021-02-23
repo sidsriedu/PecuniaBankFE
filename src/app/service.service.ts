9054dea2a63115bbe2e7214871d1c52f41f45f9e
sidsriedu <sidsriedu@gmail.com>
2021-02-23 18:53:53 +0530

    Url Update

--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -19,31 +19,31 @@ export class ServiceService {
 
 
   public validateEmail(username: any, password: any) {
-    return this.http.get("http://localhost:8085/login/validate/" + username + "/" + password, { responseType: 'text' });
+    return this.http.get("https://pecunia-bank-be.herokuapp.com/login/validate/" + username + "/" + password, { responseType: 'text' });
   }
   public getbalance(accountId: string) {
     console.log(accountId);
-    return this.http.get("http://localhost:8085/balance/getAccountbyID/" + accountId, { responseType: 'json' });
+    return this.http.get("https://pecunia-bank-be.herokuapp.com/balance/getAccountbyID/" + accountId, { responseType: 'json' });
   }
 
   public AddAccount(cus) {
-    return this.http.post("http://localhost:8085/AccountManagement/create", cus, { responseType: 'text' });
+    return this.http.post("https://pecunia-bank-be.herokuapp.com/AccountManagement/create", cus, { responseType: 'text' });
   }
   public ShowAccountDetails(accountId: string){
-    return this.http.get("http://localhost:8085/AccountManagement/find/" + accountId, { responseType: 'json' })
+    return this.http.get("https://pecunia-bank-be.herokuapp.com/AccountManagement/find/" + accountId, { responseType: 'json' })
   }
   public UpdateAccount(accountId: string, customerName: string, contactNumber: string, address: string) {
     console.log("in service" + accountId);
-    return this.http.put("http://localhost:8085/AccountManagement/update/" + accountId + "/" + customerName + "/" + contactNumber + "/" + address, { responseType: 'text' });
+    return this.http.put("https://pecunia-bank-be.herokuapp.com/AccountManagement/update/" + accountId + "/" + customerName + "/" + contactNumber + "/" + address, { responseType: 'text' });
   }
   public DeleteAccount(accountId: string) {
-    return this.http.delete("http://localhost:2001/AccountManagement/delete/" + accountId, { responseType: 'text' });
+    return this.http.delete("https://pecunia-bank-be.herokuapp.com/AccountManagement/delete/" + accountId, { responseType: 'text' });
   }
   updatePassbook(accountId:string){
-    return this.http.get("http://localhost:8085/passbook/updatePassbook/"+accountId ,{responseType: 'json'});
+    return this.http.get("https://pecunia-bank-be.herokuapp.com/updatePassbook/"+accountId ,{responseType: 'json'});
   }
   accountSummary(accountId:string,StartDate:Date,EndDate:Date){
-    return this.http.get("http://localhost:8085/passbook/accountSummary/"+accountId+"/"+StartDate+"/"+EndDate ,{responseType: 'json'});
+    return this.http.get("https://pecunia-bank-be.herokuapp.com/accountSummary/"+accountId+"/"+StartDate+"/"+EndDate ,{responseType: 'json'});
   }
   updateBal(updatebal: LoanDisbursal):any {
     const headers =new HttpHeaders().set('Content_Type', 'text/plain ;charset=utf-8');
